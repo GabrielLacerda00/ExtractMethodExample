60b97d7ffaf063caf8df7935a147cd831f8d9329
GabrielLacerda00 <gabriel.lacerd.gr@gmail.com>
2023-06-05 08:47:16 -0300

    Criando calculadora

---
+++ Calculadora.java
@@ -0,0 +1,23 @@
+public class Calculadora {
+  public int calcular(String operacao, int num1, int num2) {
+    int resultado = 0;
+
+    if (operacao.equals("soma")) {
+      resultado = num1 + num2;
+    } else if (operacao.equals("subtracao")) {
+      resultado = num1 - num2;
+    } else if (operacao.equals("multiplicacao")) {
+      resultado = num1 * num2;
+    } else if (operacao.equals("divisao")) {
+      if (num2 != 0) {
+        resultado = num1 / num2;
+      } else {
+        throw new IllegalArgumentException("Divisão por zero não é permitida");
+      }
+    } else {
+      throw new IllegalArgumentException("Operação desconhecida: " + operacao);
+    }
+
+    return resultado;
+  }
+}
